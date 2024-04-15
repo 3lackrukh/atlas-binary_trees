b1c166e9f4ff270f8351ff24b9dbacea77bc4b0f
3lackrukh <nathan.rhys@atlasschool.com>
2024-04-15 21:02:01 +0000

    corrections to betty style

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,7 +4,7 @@
  * binary_tree_node- creates a binary tree node.
  * @parent: pointer to the parent node of the node to create.
  * @value: the value to be stored in the new node.
- * 
+ *
  * Return: a pointer to the new node. NULL on failure.
 */
 
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
     binary_tree_t *new_node;
 
     new_node = malloc(sizeof(binary_tree_t));
-    if(!new_node)
+    if (!new_node)
         return (NULL);
 
     new_node->n = value;
