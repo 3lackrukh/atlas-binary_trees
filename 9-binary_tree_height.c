6acf00c832901d9e928c358bf413eb2c82f11952
3lackrukh <nathan.rhys@atlasschool.com>
2024-04-15 23:58:03 +0000

    removed trailing whitespace line 23

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -20,7 +20,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree->right)
 		r_height = 1 + binary_tree_height(tree->right);
-	
+
 	if (l_height >= r_height)
 		return (l_height);
 	else
