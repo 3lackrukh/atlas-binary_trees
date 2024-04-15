3a91c03e0d0147485baefe25e0564dfc1febd038
3lackrukh <nathan.rhys@atlasschool.com>
2024-04-15 23:39:42 +0000

    created 9-binary_tree_height.c recursive addition with traversal of left and right, final iteration returns the greater of left or right values.

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height- measures the height of a binary tree.
+ *
+ * @tree: a pointer to the root node of the tree.
+ *
+ * Return: a size_t measure of the height.
+*/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t r_height, l_height;
+
+	if (!tree)
+		return (0);
+
+	l_height = binary_tree_height(tree->left);
+	r_height = binary_tree_height(tree->right);
+	if (l_height >= r_height)
+		return (l_height + 1);
+	else
+		return (r_height + 1);
+}
