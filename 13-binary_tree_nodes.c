b29eaf8ea5c9f5315d318fcce677ae927faf2fb2
3lackrukh <nathan.rhys@atlasschool.com>
2024-04-16 02:30:20 +0000

    changed return stmt line 20 to increment parents count. line 22 parents += sum of recursive calls to left and right brances.

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -17,9 +17,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left || tree->right)
-		return (1);
+		parents++;
 	
-	parents = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
+	parents += binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
 
 	return (parents);
 }
