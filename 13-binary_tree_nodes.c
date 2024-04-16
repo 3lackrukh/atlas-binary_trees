1c72487d604e4975fa1e714dbd7bad92b6ffc8d6
3lackrukh <nathan.rhys@atlasschool.com>
2024-04-16 02:31:46 +0000

    removed trailing whitespace line 21

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -18,7 +18,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	if (tree->left || tree->right)
 		parents++;
-	
+
 	parents += binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
 
 	return (parents);
