20a03e960258af167bd2ef62c1f60d7285021128
3lackrukh <nathan.rhys@atlasschool.com>
2024-04-16 03:07:31 +0000

    added a version of binary_tree_height to binary_tree_balance. Added height variant to header file.

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,10 +16,37 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
-		l_height = 1 + binary_tree_balance(tree->left);
+		l_height = 1 + binary_tree_h(tree->left);
 
 	if (tree->right)
-		r_height = 1 + binary_tree_balance(tree->right);
+		r_height = 1 + binary_tree_h(tree->right);
 
 	return (l_height - r_height);
+}
+
+/**
+ * binary_tree_h- measures the height of a binary tree.
+ *
+ * @tree: a pointer to the root node of the tree.
+ *
+ * Return: an int measure of the height.
+*/
+
+int binary_tree_h(const binary_tree_t *tree)
+{
+	int r_height = 0, l_height = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		l_height = 1 + binary_tree_h(tree->left);
+
+	if (tree->right)
+		r_height = 1 + binary_tree_h(tree->right);
+
+	if (l_height >= r_height)
+		return (l_height);
+	else
+		return (r_height);
 }
