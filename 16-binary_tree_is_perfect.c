33b4509c9bd9076a39828902a1da887fe7795a56
3lackrukh <nathan.rhys@atlasschool.com>
2024-04-18 23:33:10 +0000

    split line 19 into two statements now betty compliant

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -16,7 +16,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 if (tree->left == NULL && tree->right == NULL)
 	return (1);
 
-if (!binary_tree_is_perfect(tree->left) || !binary_tree_is_perfect(tree->right))
+if (!binary_tree_is_perfect(tree->left))
+	return (0);
+
+if (!binary_tree_is_perfect(tree->right))
 	return (0);
 
 if (binary_tree_h(tree->left) == binary_tree_h(tree->right))
