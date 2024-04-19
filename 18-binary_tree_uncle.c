00425e126e336fd288526be7332edb631934ca56
3lackrukh <nathan.rhys@atlasschool.com>
2024-04-19 00:02:22 +0000

    removed trailing whitespace lines 5, 7, 15

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,9 +2,9 @@
 
 /**
  * binary_tree_uncle- finds the uncle of a node in a binary tree.
- * 
+ *
  * @node: a pointer to the node to find the uncle of.
- * 
+ *
  * Return: a pointer to the uncle node.
 */
 
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
-	
+
 	if (node->parent == node->parent->parent->left)
 		return (node->parent->parent->right);
 	else
