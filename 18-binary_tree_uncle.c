f9f8ea1c920df315ed981168e876d3612b0ea0c5
3lackrukh <nathan.rhys@atlasschool.com>
2024-04-19 00:00:30 +0000

    created 18-binary_tree_uncle.c if parent = parent parent left, return parent parent right, else return parent parent left.

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle- finds the uncle of a node in a binary tree.
+ * 
+ * @node: a pointer to the node to find the uncle of.
+ * 
+ * Return: a pointer to the uncle node.
+*/
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent || !node->parent->parent)
+		return (NULL);
+	
+	if (node->parent == node->parent->parent->left)
+		return (node->parent->parent->right);
+	else
+		return (node->parent->parent->left);
+}
