eb66eab7a6a8de397918966ea6e544562dba96cc
3lackrukh <nathan.rhys@atlasschool.com>
2024-04-20 00:35:04 +0000

    removed trailing whitespaces and changed spaces to tabs.

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -2,41 +2,41 @@
 
 /**
  * binary_tree_levelorder- traverses a binary tree in level-order.
- * 
+ *
  * @tree: a pointer to the root node of the tree.
  * @func: a pointer to the function to call for each node.
- * 
+ *
  * Return: nothing.
 */
 
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-    int front = 0, rear = 0;
-    binary_tree_t **queue = NULL;
-    binary_tree_t *current;
+	int front = 0, rear = 0;
+	binary_tree_t **queue = NULL;
+	binary_tree_t *current;
 	size_t nodes;
 
-    if (!tree || !func)
-        return;
+	if (!tree || !func)
+		return;
 
-    nodes = binary_tree_size(tree);
-    queue = (binary_tree_t **)malloc(nodes * sizeof(binary_tree_t*));
-        if (!queue)
-            return;
+	nodes = binary_tree_size(tree);
+	queue = (binary_tree_t **)malloc(nodes * sizeof(binary_tree_t*));
+		if (!queue)
+			return;
 
-    queue[rear++] = (binary_tree_t *)tree;
-    while (front != rear)
-    {
-        current = queue[front++];
-        func(current->n);
+	queue[rear++] = (binary_tree_t *)tree;
+	while (front != rear)
+	{
+		current = queue[front++];
+		func(current->n);
 
-        if(current->left)
-            queue[rear++] = current->left;
-        
-        if (current->right)
-            queue[rear++] = current->right;
-    }
-    free(queue);
+		if(current->left)
+			queue[rear++] = current->left;
+
+		if (current->right)
+			queue[rear++] = current->right;
+	}
+	free(queue);
 }
 
 #include "binary_trees.h"
